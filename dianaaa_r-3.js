420a9c9377181e6e382b3bcd8d4fda7afce2c6bd
Diana <romanyukdiana13082006@gmail.com>
2023-11-08 19:36:23 +0200

    Added task 3 from hw-2

---
+++ dianaaa_r-3.js
@@ -0,0 +1,25 @@
+function createAnagrams(words) {
+    const anagrams = {};
+    words.forEach((item) => {
+        const sortedWord = item.split('').sort().join('');
+        if (anagrams[sortedWord]) {
+            anagrams[sortedWord].push(item);
+        } else {
+            anagrams[sortedWord] = [item];
+        }
+    });
+    return anagrams;
+}
+
+function groupAnagrams(words) {
+    const groups = [];
+    const distribute = createAnagrams(words);
+
+    for (const sortedWord in distribute) {
+        groups.push(distribute[sortedWord]);
+    }
+    return groups;
+}
+
+const words = ["rat", "tar", "star"];
+console.log(groupAnagrams(words));
